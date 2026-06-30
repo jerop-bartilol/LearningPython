ff83879d7f1778f139bd7be75acb11edb25d0612
jerop-bartilol <jeropbartilo@gmail.com>
2026-06-30 23:48:44 +0300

    Learning python

---
+++ first.py
@@ -0,0 +1,34 @@
+#First 
+#number= int(input("Enter Number"))
+#if number %2==0:
+ #   print(f"{number}\t is an even number")
+#else:
+ #   print(f"{number}\t is an odd number")
+    
+#Second
+
+#cat1= float(input("Enter mark"))
+#cat2= float(input("Enter mark"))
+#Exam= float(input("Enter mark"))
+#finalScore=cat1+cat2+Exam
+
+#if finalScore>=70 and finalScore<=100:
+ #    grade= "A"
+#if finalScore>=60 and finalScore<=69:
+ #   grade="B"
+#if finalScore>=50 and finalScore<=59:
+  #  grade="C"
+#if finalScore>=40 and finalScore<=49:
+ #   grade="D"
+#if finalScore >=0 and finalScore<=39:
+ #   grade="F"
+
+#print(f"Final Score is {finalScore} and grade is {grade}")
+
+#Third
+x= "apple", "banana" , "onion" , "blue", "green"
+
+book ={
+  "ISBNO":1234
+  
+}
